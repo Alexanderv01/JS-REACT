4e906e262b581f44dc0a7af3ce7e4437dbaa393e
Alexanderv01 <86639841+Alexanderv01@users.noreply.github.com>
2021-09-12 21:10:42 -0500

    Initial commit

---
+++ arrays.js
@@ -0,0 +1,9 @@
+const numero = [20,40,20,80,100]
+const alumnos = [
+    {nombre: "Yadi",n1:20,n2:50},
+    {nombre: "Erick",n1:50,n2:50},
+    {nombre: "Daniel",n1:40,n2:50},
+    {nombre: "Dani",n1:30,n2:40},
+    {nombre: "Noris",n1:50,n2:40},
+    {nombre: "Chiky",n1:50,n2:45},
+]
